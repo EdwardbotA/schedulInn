6ef231540e6f6899451e8015f224bc854d206150
Edward B <ebotelloaroca00@gmail.com>
2025-02-20 00:34:16 -0500

    Se genera enrutado privado

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,24 @@
-import { HashRouter, Route, Routes } from "react-router";
-import App from "./App";
+import { HashRouter, Navigate, Route, Routes } from "react-router";
 import BasePage from "./pages/BasePage";
+import HomePage from "./pages/HomePage";
+import Dashboard from "./pages/Dashboard";
+import { useAuth } from "./context/context";
+
+const PrivateRoute = ({ element }) => {
+  const { user } = useAuth();
+  return user ? element : <Navigate to="/" />;
+};
 
 function AppRoutes() {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<BasePage />}>
-          <Route index element={<App />} />
+          <Route index element={<HomePage />} />
+          <Route
+            path="dashboard"
+            element={<PrivateRoute element={<Dashboard />} />}
+          />
         </Route>
       </Routes>
     </HashRouter>
