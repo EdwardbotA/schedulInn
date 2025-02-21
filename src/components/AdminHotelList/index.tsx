f0b1d52cf31079ee8b3fcfdd4cdf2bd35ee8a790
Edward B <ebotelloaroca00@gmail.com>
2025-02-20 23:39:10 -0500

    se crea la vista de admin donde se ven los hoteles

---
+++ src/components/AdminHotelList/index.tsx
@@ -0,0 +1,106 @@
+import { FC, useState } from "react";
+import { useAuth } from "../../context/context";
+import { useQuery } from "@tanstack/react-query";
+import { fetchHotelesAdmin } from "../../services/hotelAPI/hotelsAdminAPI";
+import IHotelAdminData from "../../interface/IHotelAdminData";
+import Button from "../Button";
+import { Link } from "react-router";
+
+const AdminHotelList: FC = () => {
+  const { user } = useAuth();
+  const [openHotel, setOpenHotel] = useState<string | null>(null);
+  const [openHotelRoom, setOpenHotelRoom] = useState<string | null>(null);
+
+  const {
+    data: hotels,
+    isLoading,
+    error,
+  } = useQuery<IHotelAdminData[]>({
+    queryKey: ["hotelsAdmin", user!.id],
+    queryFn: () => fetchHotelesAdmin(user!.id),
+    enabled: !!user?.id,
+  });
+
+  if (isLoading) return <p>Cargando...</p>;
+  if (error) return <p>Error al cargar los hoteles</p>;
+
+  return (
+    <div>
+      {hotels!.map((hotel: IHotelAdminData) => (
+        <article
+          key={hotel.id}
+          className="p-4 shadow-md rounded-lg border border-gray-300"
+        >
+          <div
+            className="flex justify-between items-center cursor-pointer"
+            onClick={() =>
+              setOpenHotel(openHotel === hotel.id ? null : hotel.id)
+            }
+          >
+            <div>
+              <h2 className="text-lg font-bold">{hotel.nombre}</h2>
+              <p className="text-sm text-gray-600">{hotel.direccion}</p>
+              <p className="text-sm text-gray-600">Tel: {hotel.telefono}</p>
+            </div>
+            {openHotel === hotel.id ? "🔺" : "🔻"}
+          </div>
+          {openHotel === hotel.id && (
+            <article className="mt-2 space-y-2">
+              <h3 className="text-md font-semibold">Habitaciones:</h3>
+
+              {hotel.habitaciones.length > 0 ? (
+                hotel.habitaciones.map((habitacion) => (
+                  <div
+                    key={habitacion.id}
+                    className="border p-2 rounded-md bg-gray-100"
+                    onClick={() =>
+                      setOpenHotelRoom(
+                        openHotelRoom === habitacion.id ? null : habitacion.id
+                      )
+                    }
+                  >
+                    <p>
+                      <strong>Tipo:</strong> {habitacion.tipo}
+                    </p>
+                    <p>
+                      <strong>Costo Base:</strong> ${habitacion.costoBase}
+                    </p>
+                    <p>
+                      <strong>Impuestos:</strong> ${habitacion.impuesto}
+                    </p>
+                    <p>
+                      <strong>Ubicación:</strong> {habitacion.ubicacion}
+                    </p>
+                    <p
+                      className={
+                        !habitacion.habilitado
+                          ? "text-green-600"
+                          : "text-red-600"
+                      }
+                    >
+                      {!habitacion.habilitado ? "Disponible" : "No Disponible"}
+                    </p>
+                    {openHotelRoom === habitacion.id && (
+                      <Link
+                        to={`/dashboard/editar-habitacion/${habitacion.id}`}
+                      >
+                        <Button>Editar Habitación</Button>
+                      </Link>
+                    )}
+                  </div>
+                ))
+              ) : (
+                <p>No hay habitaciones</p>
+              )}
+              <Link to={`/dashboard/editar-hotel/${hotel.id}`}>
+                <Button>Editar Hotel</Button>
+              </Link>
+            </article>
+          )}
+        </article>
+      ))}
+    </div>
+  );
+};
+
+export default AdminHotelList;
