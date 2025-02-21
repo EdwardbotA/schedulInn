6de249b9f5f1d5b1ccbf0837c9b053de69ecadd6
Edward B <ebotelloaroca00@gmail.com>
2025-02-20 23:49:00 -0500

    se genera la vista del dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,18 +1,40 @@
-// import { useQuery } from "@tanstack/react-query";
 import { FC } from "react";
-// import { fetchUser } from "../../services/usersAPI/userAPI";
+import { useAuth } from "../../context/context";
+import { Link, Outlet, useLocation, useNavigate } from "react-router";
+import AdminHotelList from "../../components/AdminHotelList";
+import Button from "../../components/Button";
 
 const Dashboard: FC = () => {
-  // const { data: users, error } = useQuery({
-  //   queryKey: ["users"],
-  //   queryFn: () => fetchUser("juanperez@example.com", "viajero"),
-  // });
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-  // if (error) {
-  //   return <div>Error al cargar usuario</div>;
-  // }
+  if (!user) {
+    navigate("/");
+  }
 
-  return <div></div>;
+  return (
+    <section className="flex flex-col gap-5 w-full py-7 lg:max-w-5xl lg:self-center">
+      <div className="flex flex-col w-full gap-4 md:flex-row">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-2xl font-bold">Dashboard</h1>
+          <p className="text-xl">Hola {user!.nombre}</p>
+        </div>
+        <div className="flex gap-2 items-center justify-center grow">
+          <Link to={`/dashboard/agregar-hotel`}>
+            <Button>Agregar Hotel</Button>
+          </Link>
+          <Link to={`/dashboard/reservas`}>
+            <Button>Ver Reservas</Button>
+          </Link>
+        </div>
+      </div>
+      {location.pathname === "/dashboard" && user!.tipo === "agente" && (
+        <AdminHotelList />
+      )}
+      <Outlet />
+    </section>
+  );
 };
 
 export default Dashboard;
