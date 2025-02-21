7239913408c18378401953b02707e79d53dd4134
Edward B <ebotelloaroca00@gmail.com>
2025-02-21 16:36:07 -0500

    Agrega forma para volver al dashboard desde el logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,22 @@
 import { FC } from "react";
 import schedulInnLogo from "../../assets/schedulInnLogo.png";
 import Button from "../Button";
-import { useLocation } from "react-router";
+import { Link, useLocation } from "react-router";
 import { useAuth } from "../../context/context";
 
 const Header: FC = () => {
-  const { logout } = useAuth();
+  const { user, logout } = useAuth();
   const location = useLocation();
 
   return (
     <header className="flex w-full justify-between">
-      <img src={schedulInnLogo} alt="Logo de SchedulInn" className="h-9" />
+      <Link to={`${user ? "/dashboard" : "/"}`}>
+        <img
+          src={schedulInnLogo}
+          alt="Logo de SchedulInn"
+          className="h-9 hover:scale-110 transition-transform"
+        />
+      </Link>
       {location.pathname.includes("dashboard") && (
         <Button handleClick={logout}>Log out</Button>
       )}
