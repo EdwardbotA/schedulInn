ab0ac5bd0e3293d216bdee8170bbb7601ba40f62
Edward B <ebotelloaroca00@gmail.com>
2025-02-20 23:39:41 -0500

    agrega boton para cerrar sesion

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,19 @@
 import { FC } from "react";
 import schedulInnLogo from "../../assets/schedulInnLogo.png";
+import Button from "../Button";
+import { useLocation } from "react-router";
+import { useAuth } from "../../context/context";
 
 const Header: FC = () => {
+  const { logout } = useAuth();
+  const location = useLocation();
+
   return (
-    <header className="flex  w-full">
+    <header className="flex w-full justify-between">
       <img src={schedulInnLogo} alt="Logo de SchedulInn" className="h-9" />
+      {location.pathname.includes("dashboard") && (
+        <Button handleClick={logout}>Log out</Button>
+      )}
     </header>
   );
 };
