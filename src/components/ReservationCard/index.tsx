d1dd5b4f54c68ff84f5ebf55e5f40e64c60285dd
Edward B <ebotelloaroca00@gmail.com>
2025-02-21 17:42:10 -0500

    Agrega Componente de ReservationCard

---
+++ src/components/ReservationCard/index.tsx
@@ -0,0 +1,61 @@
+import { FC } from "react";
+import IReservationData from "../../interface/IReservationData";
+import IHotelAdminData from "../../interface/IHotelAdminData";
+import { useQuery } from "@tanstack/react-query";
+import { useAuth } from "../../context/context";
+import { fetchHotelesAdmin } from "../../services/hotelAPI/hotelsAdminAPI";
+import Button from "../Button";
+
+interface IReservationCardProps {
+  reservation: IReservationData;
+}
+
+const ReservationCard: FC<IReservationCardProps> = ({ reservation }) => {
+  const { user } = useAuth();
+  const { data: hotels } = useQuery<IHotelAdminData[]>({
+    queryKey: ["hotelsAdmin", user!.id],
+    queryFn: () => fetchHotelesAdmin(user!.id),
+    enabled: !!user?.id,
+  });
+
+  const rooms = hotels
+    ?.map((hotel) => {
+      if (hotel.habitaciones.length === 0) return [];
+
+      return {
+        room: hotel.habitaciones,
+        hotelId: hotel.id,
+      };
+    })
+    .flat();
+
+  return (
+    <article className="flex flex-col shadow-lg rounded-lg p-6 border gap-5">
+      <h2 className="text-xl font-semibold text-gray-800">
+        Reserva #{reservation.id} hotel:{" "}
+        {hotels?.find((hotel) => hotel.id === reservation.hotelId)?.nombre}
+      </h2>
+      <div className="mt-2 text-gray-600">
+        <p className="font-medium">
+          Inicio de reserva: {reservation.fechaEntrada}
+        </p>
+        <p className="font-medium">fin de reserva: {reservation.fechaSalida}</p>
+        <p className="font-medium">
+          Nombre del los huespedes:{" "}
+          {reservation.huespedes.map((huespede) => huespede.nombre).join(", ")}
+        </p>
+        <p className="font-medium">
+          habitacion:{" "}
+          {
+            rooms
+              ?.find((room) => room.hotelId === reservation.hotelId)
+              ?.room.find((bed) => bed.id === reservation.habitacionId)?.tipo
+          }
+        </p>
+      </div>
+      <Button>Ver Detalles</Button>
+    </article>
+  );
+};
+
+export default ReservationCard;
