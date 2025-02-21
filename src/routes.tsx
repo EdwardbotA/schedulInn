0ac2a3777c8b99c47f5ede9451a1951dcf59c1d5
Edward B <ebotelloaroca00@gmail.com>
2025-02-20 23:38:14 -0500

    Se generarn rutas para poder editar hoteles, habitaciones agregar hoteles y ver las reservas

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage";
 import Dashboard from "./pages/Dashboard";
 import { useAuth } from "./context/context";
 import { JSX } from "react";
+import Reservations from "./pages/Reservation";
 
 interface IPrivateRoute {
   element: JSX.Element;
@@ -23,7 +24,21 @@ function AppRoutes() {
           <Route
             path="dashboard"
             element={<PrivateRoute element={<Dashboard />} />}
-          />
+          >
+            <Route
+              path="agregar-hotel"
+              element={<h1>Agregar nuevo hotel</h1>}
+            />
+            <Route
+              path="editar-hotel/:hotelId"
+              element={<h1>Editar hotel</h1>}
+            />
+            <Route
+              path="editar-habitacion/:habitacionId"
+              element={<h1>Editar habitacion</h1>}
+            />
+            <Route path="reservas" element={<Reservations />} />
+          </Route>
           <Route path="*" element={<PrivateRoute element={<Dashboard />} />} />
         </Route>
       </Routes>
