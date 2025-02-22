eda063d1425f5c375c3525c726171be380e35706
Edward B <ebotelloaroca00@gmail.com>
2025-02-21 20:40:49 -0500

    Agrega logica para obtener un hotel, actuaslizar sus datos y borrarlo

--- src/services/hotelAPI/hotelsAdminAPI.ts
+++ src/services/hotelAPI/hotelsAdminAPI.ts
@@ -17,3 +17,31 @@ export const addHotel = async (hotelData: Omit<IHotelAdminData, "id">) => {
 
   return data;
 };
+
+export const fetchHotel = async (hotelId: string | undefined) => {
+  const { data } = await axios.get(
+    `http://localhost:3001/hotelesAdmin/${hotelId}`
+  );
+
+  return data;
+};
+
+export const updateHotel = async (
+  hotelId: string,
+  hotelData: Omit<IHotelAdminData, "id" | "adminId" | "habitaciones">
+) => {
+  const { data } = await axios.patch(
+    `http://localhost:3001/hotelesAdmin/${hotelId}`,
+    hotelData
+  );
+
+  return data;
+};
+
+export const deleteHotel = async (hotelId: string) => {
+  const { data } = await axios.delete(
+    `http://localhost:3001/hotelesAdmin/${hotelId}`
+  );
+
+  return data;
+};
