bef6ede6b168f2baafd5cb329c5bfd2dd75157ad
Edward B <ebotelloaroca00@gmail.com>
2025-02-23 17:53:04 -0500

    Mejora al Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,17 +4,19 @@ interface ButtonProps {
   children: ReactNode;
   handleClick?: () => void;
   isDisabled?: boolean;
+  type?: "submit" | "button";
 }
 
 const Button: FC<ButtonProps> = ({
   children,
   isDisabled = false,
   handleClick,
+  type = "submit",
 }) => {
   return (
     <button
       onClick={handleClick}
-      type="submit"
+      type={type}
       disabled={isDisabled}
       className="bg-white border-2 border-primary cursor-pointer text-primary p-2 px-6 rounded text-base font-semibold hover:bg-primary hover:text-white disabled:bg-gray-300 disabled:text-gray-900 disabled:cursor-not-allowed"
     >
