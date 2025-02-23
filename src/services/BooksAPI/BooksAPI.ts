8c7846a48f3ca9bb5d24cd112a0b7516ac5357f5
Edward B <ebotelloaroca00@gmail.com>
2025-02-23 17:52:50 -0500

    Envio de datos a endpoint de reservas

--- src/services/BooksAPI/BooksAPI.ts
+++ src/services/BooksAPI/BooksAPI.ts
@@ -1,8 +1,20 @@
 import axios from "axios";
+import IReservationData from "../../interface/IReservationData";
 
 export const fetchBooks = async (hotelId: string) => {
   const { data } = await axios.get(
     `http://localhost:3001/reservas?hotelId=${hotelId}`
+  );
+
+  return data;
+};
+
+export const addReservation = async (
+  reservationData: Omit<IReservationData, "id">
+) => {
+  const { data } = await axios.post(
+    "http://localhost:3001/reservas",
+    reservationData
   );
 
   return data;
