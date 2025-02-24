e51ceebc462b2ff47e5c9135eb2e5505cdc29100
Edward B <ebotelloaroca00@gmail.com>
2025-02-23 20:34:26 -0500

    Agrega logica para el envio de correos

--- src/services/BooksAPI/BooksAPI.ts
+++ src/services/BooksAPI/BooksAPI.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 import IReservationData from "../../interface/IReservationData";
+import { sendConfirmationEmail } from "../../utils/sendEmail";
+import IUserData from "../../interface/IUserData";
+import { fetchHotel } from "../hotelAPI/hotelsAdminAPI";
 
-export const fetchBooks = async (hotelId: string) => {
+export const fetchBooksAdmin = async (hotelId: string) => {
   const { data } = await axios.get(
     `http://localhost:3001/reservas?hotelId=${hotelId}`
   );
@@ -9,13 +12,32 @@ export const fetchBooks = async (hotelId: string) => {
   return data;
 };
 
+export const fetchBooksGuest = async (userId: string) => {
+  const { data } = await axios.get(
+    `http://localhost:3001/reservas?usuarioId=${userId}`
+  );
+
+  return data;
+};
+
 export const addReservation = async (
-  reservationData: Omit<IReservationData, "id">
+  reservationData: Omit<IReservationData, "id">,
+  user: IUserData
 ) => {
   const { data } = await axios.post(
     "http://localhost:3001/reservas",
     reservationData
   );
+
+  const hotel = await fetchHotel(reservationData.hotelId);
+
+  await sendConfirmationEmail(user.email, {
+		from_name: 'SchedulInn',
+    nombre: user.nombre,
+    hotel: hotel.nombre,
+    fechaEntrada: reservationData.fechaEntrada,
+    fechaSalida: reservationData.fechaSalida,
+  });
 
   return data;
 };
---
+++ src/utils/sendEmail.ts
@@ -0,0 +1,27 @@
+import emailjs from "@emailjs/browser";
+
+const SERVICE_ID = import.meta.env.VITE_SERVICE_ID;
+const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID;
+const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY;
+
+interface IEmailData {
+  from_name: string;
+  nombre: string;
+  hotel: string;
+  fechaEntrada: string;
+  fechaSalida: string;
+  [key: string]: unknown;
+}
+
+export const sendConfirmationEmail = async (
+  email: string,
+  data: IEmailData
+) => {
+  try {
+    await emailjs.send(SERVICE_ID, TEMPLATE_ID, data, PUBLIC_KEY);
+
+    console.log("correo enviado con exito");
+  } catch (error) {
+    console.error("error al enviar correo", error);
+  }
+};
